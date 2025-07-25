e8a887ec34e8c02b884a75bbaa97f55cbae908c2
Luan Lima <lluan.lima94@gmail.com>
2025-07-25 10:16:11 -0300

    add recipe

--- src/backend/MyRecipeBook.Domain/Entities/Instruction.cs
+++ src/backend/MyRecipeBook.Domain/Entities/Instruction.cs
@@ -1,12 +1,13 @@
-﻿using MyRecipeBook.Domain.Enums;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyRecipeBook.Domain.Entities
 {
+    [Table("Instructions")]
     public class Instruction : EntityBase
     {
         public int Step { get; set; }
         public string Description { get; set; } = string.Empty;
         public long RecipeId { get; set; }
-       
+
     }
 }
