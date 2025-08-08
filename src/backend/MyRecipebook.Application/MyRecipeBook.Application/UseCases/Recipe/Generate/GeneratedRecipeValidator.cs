7ae8e9dedcf868299606066a4e873f0511c58b2c
Luan Lima <lluan.lima94@gmail.com>
2025-08-07 23:25:01 -0300

    Refactor field name usage in GeneratedRecipeValidator
    Replaced hardcoded string literals for the "Ingredients" field with a constant `INGREDIENTS_FIELD` in `GeneratedRecipeValidator.cs`. This change enhances maintainability and consistency in error messages for validation rules.

--- src/backend/MyRecipebook.Application/MyRecipeBook.Application/UseCases/Recipe/Generate/GeneratedRecipeValidator.cs
+++ src/backend/MyRecipebook.Application/MyRecipeBook.Application/UseCases/Recipe/Generate/GeneratedRecipeValidator.cs
@@ -7,7 +7,7 @@ namespace MyRecipeBook.Application.UseCases.Recipe.Generate
 {
     public class GeneratedRecipeValidator : AbstractValidator<RequestGenerateRecipeJson>
     {
-
+        private const string INGREDIENTS_FIELD = "Ingredients";
         public GeneratedRecipeValidator()
         {
             var maximum_number_ingredients = MyRecipeBookRuleConstants.MAXIMUM_INGREDIENTS_GENERATE_RECIPE;
@@ -15,32 +15,31 @@ namespace MyRecipeBook.Application.UseCases.Recipe.Generate
 
             RuleFor(r => r.Ingredients.Count)
                 .InclusiveBetween(minimum_number_ingredients, maximum_number_ingredients)
-                .WithMessage(ResourceMessageHelper.FieldOutOfRange(fieldName: "Ingredients", firstValue: minimum_number_ingredients.ToString(), secondValue: maximum_number_ingredients.ToString()));
+                .WithMessage(ResourceMessageHelper.FieldOutOfRange(fieldName: INGREDIENTS_FIELD, firstValue: minimum_number_ingredients.ToString(), secondValue: maximum_number_ingredients.ToString()));
 
             RuleFor(r => r.Ingredients)
                 .Must(ingredients => ingredients.Count == ingredients.Distinct().Count())
-                .WithMessage(ResourceMessageHelper.FieldDuplicateValue(fieldName: "Ingredients"));
+                .WithMessage(ResourceMessageHelper.FieldDuplicateValue(fieldName: INGREDIENTS_FIELD));
 
             RuleFor(r => r.Ingredients)
                 .ForEach(rule => rule.Custom(
                     (value, context) => GetRule(value, context)
                     )
                 )
-                .WithMessage(ResourceMessageHelper.FieldDuplicateValue(fieldName: "Ingredients"));
+                .WithMessage(ResourceMessageHelper.FieldDuplicateValue(fieldName: INGREDIENTS_FIELD));
         }
 
         private static void GetRule(string value, ValidationContext<IEnumerable<string>> context)
         {
             if (string.IsNullOrWhiteSpace(value))
             {
-                context.AddFailure("Ingredient", ResourceMessageHelper.FieldEmpty(fieldName: "Ingredients"));
+                context.AddFailure(INGREDIENTS_FIELD, ResourceMessageHelper.FieldEmpty(fieldName: INGREDIENTS_FIELD));
                 return;
             }
 
             if (value.Count(c => c == ' ') > 3 || value.Count(c => c == '/') > 1)
             {
-                context.AddFailure("Ingredient", ResourceMessageHelper.FieldPatternMismatch(fieldName: "Ingredients", pattern: "value1, value2 or 3/4 value2"));
-                return;
+                context.AddFailure(INGREDIENTS_FIELD, ResourceMessageHelper.FieldPatternMismatch(fieldName: INGREDIENTS_FIELD, pattern: "value1, value2 or 3/4 value2"));
             }
 
         }
